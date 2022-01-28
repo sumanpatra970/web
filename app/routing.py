96856fc60c817986b850157fce80b65cfdeba993
sumanpatra970 <patrasuman992@gmail.com>
2022-01-28 07:32:58 +0530

    n

--- app/routing.py
+++ app/routing.py
@@ -4,6 +4,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-     path('wa/sc/<str:grpname>',consumer.MySync.as_asgi()),
+    path('wa/sc/<str:grpname>',consumer.MySync.as_asgi()),
     path('wa/ac/<str:grpname>',consumer.MyAsync.as_asgi())
 ]
