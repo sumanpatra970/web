c811cedffc0546a00f64d32f86a3d4cae6946336
sumanpatra970 <patrasuman992@gmail.com>
2022-01-28 08:43:34 +0530

    nc

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
+import django
+django.setup()
 class chat(models.Model):
     content = models.CharField(max_length=255)
     group = models.CharField(max_length=50)
--- web/settings.py
+++ web/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
