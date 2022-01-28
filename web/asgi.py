b68bf4103ec1dd1779971fdd5d6c18ad99bdf251
sumanpatra970 <patrasuman992@gmail.com>
2022-01-28 08:01:40 +0530

    Update asgi.py

--- web/asgi.py
+++ web/asgi.py
@@ -9,7 +9,7 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabulator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
 
 django.setup()
 
