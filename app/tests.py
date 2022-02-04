0090a6fc7ad772326ba49386ed55e55147ec20a6
sumanpatra970 <patrasuman992@gmail.com>
2022-02-04 17:23:52 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 # Create your tests here.
+from django.contrib import auth
+
+
+class AuthTestCase(TestCase):
+    def setUp(self):
+        self.u = User.objects.create_user('test', 'test@dom.com', 'pass')
+        self.u.is_staff = True
+        self.u.is_superuser = True
+        self.u.is_active = True
+        self.u.save()
+
+    def testLogin(self):
+        self.client.login(username='test', password='pass')
