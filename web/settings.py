68effd11256304fdcbfff004ad6f5fc2bb151cbe
sumanpatra970 <patrasuman992@gmail.com>
2022-02-05 08:00:17 +0530

    nc

--- web/settings.py
+++ web/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import dj_database_url
+
 import os 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,7 +62,9 @@ DATABASES = {
         'NAME':  'db.sqlite3',
     }
 }
-
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
