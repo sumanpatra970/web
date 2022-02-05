91b74b3cca7f6f37d79ed3fb702bc4fbe11a0aba
sumanpatra970 <patrasuman992@gmail.com>
2022-02-05 16:15:42 +0530

    Update urls.py

--- web/urls.py
+++ web/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
     path('home/',views.home),
-    path('chat/',views.chat),
     path('home/k/<str:group_name>/',views.index),
     path('privacy',views.privacy),
     path('policy',views.policy),
