31808815546597eaeb54e8ad33fa37ab59896f6b
sumanpatra970 <patrasuman992@gmail.com>
2022-02-05 17:30:16 +0530

    nc

--- web/settings.py
+++ web/settings.py
@@ -62,9 +62,9 @@ DATABASES = {
         'NAME':  'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
-DATABASES['default']['CONN_MAX_AGE'] = 500
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
